b461627193c300e6bbc212d2f8e86694ba8f3b7d
moaz_abdeljalil <121466818+Moaz0009@users.noreply.github.com>
2024-02-14 19:50:03 +0200

    Create Inst_mem.vhd

---
+++ Fetch Stage/Inst_mem.vhd
@@ -0,0 +1,21 @@
+----------------------------------------------------------------------------------
+-- Company: Benha Faculty OF Engineering
+-- Engineers: Moaz Abdeljalil Salah 
+--            Nour Eldin Fares
+--            Hadeer Ibrahim
+--            Yasmeen Emad
+--            Eman Mahmoud
+-- Create Date:    17:00:29 12/22/2023 
+-- Design Name: 
+-- Module Name:    MIPS_PROCESSOR - Behavioral 
+-- Project Name: 
+-- Target Devices: 
+-- Tool versions: 
+-- Description: 
+--
+-- Dependencies: 
+--
+-- Revision: 
+-- Revision 0.01 - File Created
+-- Additional Comments: 
+--
